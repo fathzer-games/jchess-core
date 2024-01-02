c7c303bcce90e04fb11dd0fdd5f1f86a186eadc5
Astesana <jm-dev@astesana.net>
2024-01-02 11:00:59 +0100

    Adapts to games-core changes

--- src/main/java/com/fathzer/jchess/ai/JChessEngine.java
+++ src/main/java/com/fathzer/jchess/ai/JChessEngine.java
@@ -12,6 +12,7 @@ import com.fathzer.games.ai.SearchStatistics;
 import com.fathzer.games.ai.evaluation.EvaluatedMove;
 import com.fathzer.games.ai.evaluation.Evaluator;
 import com.fathzer.games.ai.evaluation.Evaluation.Type;
+import com.fathzer.games.ai.iterativedeepening.DeepeningPolicy;
 import com.fathzer.games.ai.iterativedeepening.IterativeDeepeningEngine;
 import com.fathzer.games.ai.iterativedeepening.IterativeDeepeningSearch;
 import com.fathzer.games.ai.moveselector.RandomMoveSelector;
@@ -32,7 +33,7 @@ public class JChessEngine extends IterativeDeepeningEngine<Move, Board<Move>> {
 	private Function<Board<Move>, SelectiveComparator<Move>> moveComparatorSupplier;
 	
 	public JChessEngine(Supplier<Evaluator<Move, Board<Move>>> evaluatorSupplier, int maxDepth) {
-		super(maxDepth, new TT(16, SizeUnit.MB), evaluatorSupplier);
+		super(new DeepeningPolicy(maxDepth), new TT(16, SizeUnit.MB), evaluatorSupplier);
 		setDeepeningPolicy(new JChessDeepeningPolicy(maxDepth));
 		moveComparatorSupplier = BasicMoveComparator::new;
 		setMoveSelectorBuilder(b -> {
@@ -58,49 +59,48 @@ public class JChessEngine extends IterativeDeepeningEngine<Move, Board<Move>> {
 		return super.buildExecutionContext(board);
 	}
 
-	@Override
-	protected IterativeDeepeningSearch<Move> search(Board<Move> board) {
-		final EventLogger<Move, Board<Move>> logger = getLogger();
-		if (logger instanceof DefaultEventLogger) {
-			((DefaultEventLogger)logger).cs = board.getCoordinatesSystem();
-		}
-		log.info("--- Start evaluation for {} with size={}, accuracy={}, maxDepth={}, maxTime={} ---", FENUtils.to(board), getDeepeningPolicy().getSize(), getDeepeningPolicy().getAccuracy(), getDeepeningPolicy().getDepth(), getDeepeningPolicy().getMaxTime());
-		IterativeDeepeningSearch<Move> search = super.search(board);
-		log.info("--- End of iterative evaluation returns: {}", toString(search.getBestMoves(), board.getCoordinatesSystem()));
-		return search;
-	}
-
-	public static String toString(Collection<EvaluatedMove<Move>> moves, CoordinatesSystem cs) {
-		return moves.stream().map(em -> toString(em,cs)).collect(Collectors.joining(", ", "[", "]"));
-	}
-
-	public static String toString(EvaluatedMove<Move> ev, CoordinatesSystem cs) {
-		final Type type = ev.getEvaluation().getType();
-		String value;
-		if (type==Type.EVAL) {
-			value = Integer.toString(ev.getScore());
-		} else {
-			value="M"+(ev.getEvaluation().getType()==Type.LOOSE?"-":"+")+ev.getEvaluation().getCountToEnd();
-		}
-		return ev.getContent().toString(cs)+"("+value+")";
-	}
-
-	private class DefaultEventLogger implements EventLogger<Move, Board<Move>> {
+	private class DefaultEventLogger implements EngineEventLogger<Move, Board<Move>> {
 		private CoordinatesSystem cs;
 
 		public DefaultEventLogger() {
 			super();
 		}
+		
+		@Override
+		public void logSearchStart(Board<Move> board, IterativeDeepeningEngine<Move, Board<Move>> engine) {
+			cs = board.getCoordinatesSystem();
+			log.info("--- Start evaluation for {} with size={}, accuracy={}, maxDepth={}, maxTime={} ---", FENUtils.to(board), engine.getDeepeningPolicy().getSize(), engine.getDeepeningPolicy().getAccuracy(), engine.getDeepeningPolicy().getDepth(), engine.getDeepeningPolicy().getMaxTime());
+		}
 
 		@Override
-		public void logSearch(int depth, SearchStatistics stat, SearchResult<Move> bestMoves) {
+		public void logSearchEnd(Board<Move> board, IterativeDeepeningSearch<Move> result) {
+			log.info("--- End of iterative evaluation returns: {}", toString(result.getBestMoves()));
+		}
+
+		public String toString(Collection<EvaluatedMove<Move>> moves) {
+			return moves.stream().map(em -> toString(em)).collect(Collectors.joining(", ", "[", "]"));
+		}
+
+		public String toString(EvaluatedMove<Move> ev) {
+			final Type type = ev.getEvaluation().getType();
+			String value;
+			if (type==Type.EVAL) {
+				value = Integer.toString(ev.getScore());
+			} else {
+				value="M"+(ev.getEvaluation().getType()==Type.LOOSE?"-":"+")+ev.getEvaluation().getCountToEnd();
+			}
+			return ev.getContent().toString(cs)+"("+value+")";
+		}
+
+		@Override
+		public void logSearchAtDepth(int depth, SearchStatistics stat, SearchResult<Move> bestMoves) {
 			final long duration = stat.getDurationMs();
 			final List<EvaluatedMove<Move>> cut = bestMoves.getCut();
 			log.info("{} move generations, {} moves generated, {} moves played({} from TT), {} evaluations for {} first level moves at depth {} by {} threads in {}ms -> {}",
 					stat.getMoveGenerationCount(), stat.getGeneratedMoveCount(), stat.getMovePlayedCount(), stat.getMoveFromTTPlayedCount(),
 					stat.getEvaluationCount(), bestMoves.getList().size(),
 					depth, getParallelism(), duration, cut.isEmpty()?null:cut.get(0).getEvaluation());
-			log.info("Search at depth {} returns: {}", depth, JChessEngine.toString(bestMoves.getCut(),cs));
+			log.info("Search at depth {} returns: {}", depth, toString(bestMoves.getCut()));
 		}
 
 		@Override
